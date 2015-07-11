3b1ed28458ac6414c16f9adfcc81536fe4909ec6
sonota88 <yosiot8753@gmail.com>
2015-07-11 13:11:45 +0900

    parse CSV and ouput JSON

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+function puts(){
+  console.log.apply(console, arguments);
+}
+
+function convert(text){
+  var src = $(".input").val();
+  var lines = src.split("\n");
+  var rows = _(lines).map(function(line){
+    return line.split(",");
+  });
+
+  $(".output_json").val(JSON.stringify(rows));
+}
+
+$(function(){
+
+  // Events
+
+  $(".input").on("input", convert);
+  
+  // Init
+
+  convert();
+
+});
